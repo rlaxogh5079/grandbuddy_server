2eef0b90094b94e09180d28f7bf079479cbf1503
rlaxogh5079 <rlaxogh507906@gmail.com>
2025-04-18 15:39:21 +0900

    :sparkles: task 기능 구현

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from controller.request_controller import request_controller
 from controller.reward_controller import reward_controller
 from controller.review_controller import review_controller
 from controller.match_controller import match_controller
+from controller.task_controller import task_controller
 from controller.user_controller import user_controller
 from database.connection import DBObject
 from fastapi import FastAPI
@@ -10,9 +11,10 @@ import uvicorn
 app = FastAPI()
 app.include_router(user_controller)
 app.include_router(request_controller)
-app.include_router(match_controller)
 app.include_router(review_controller)
 app.include_router(reward_controller)
+app.include_router(task_controller)
+app.include_router(match_controller)
 
 @app.on_event("startup")
 async def startup():
