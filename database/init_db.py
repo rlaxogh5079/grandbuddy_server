8fcb545150b4bb215d0f6a73fd6125ff54e60fc6
rlaxogh5079 <rlaxogh507906@gmail.com>
2025-06-11 04:36:18 +0900

    request 생성

--- database/init_db.py
+++ database/init_db.py
@@ -2,7 +2,7 @@
 import asyncio
 from database.connection import DBObject
 from model.base_class import Base
-from model import user, match, message, request, review, reward, task
+from model import user, match, message, application, request, review, reward, task
 
 async def create_all():
     await DBObject.init_async_db()
